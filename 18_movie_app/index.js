9fe75745c21b96b33debb902ff22ba03b301c536
Ayush <ayushsingh916924@gmail.com>
2023-09-02 21:57:55 +0530

    starting the sound-board project

--- 18_movie_app/index.js
+++ 18_movie_app/index.js
@@ -25,11 +25,14 @@ function showMovies(movies) {
     movies.forEach((movie) => {
         const { title, poster_path, vote_average, overview } = movie;
 
+        // Check if poster_path is available
+        const posterUrl = poster_path ? IMG_PATH + poster_path : 'https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bW92aWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60';
+
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
 
         movieEl.innerHTML = `
-          <img src="${IMG_PATH + poster_path}"
+          <img src="${posterUrl}"
             alt="${title}"
             srcset=""
           />
